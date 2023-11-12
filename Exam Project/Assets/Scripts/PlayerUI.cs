002d7afe961f62e77523e63d700eec26781090d0
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2023-11-13 01:21:49 +0200

    ui

--- Exam Project/Assets/Scripts/PlayerUI.cs
+++ Exam Project/Assets/Scripts/PlayerUI.cs
@@ -7,12 +7,20 @@ using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
+     
     [SerializeField]
     private Slider healthSlider;
     [SerializeField]
     private Slider chargeSlider;
 
     [SerializeField] private TMP_Text objectiveText;
+
+    public float showRectPosY;
+    public float hideRectPosY;
+    public float moveSpeed;
+    public bool isObjectiveOpen;
+
+    public RectTransform objectiveTransform;
 
     private PlayerInfo _playerInfo;
     private LevelManager _levelManager;
@@ -29,6 +37,11 @@ public class PlayerUI : MonoBehaviour
         chargeSlider.value = (int)(_playerInfo.charge / _playerInfo.maxCharge * 100);
         
         DisplayObjectives();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            objectiveTab();
+        }
     }
 
     void DisplayObjectives()
@@ -46,7 +59,40 @@ public class PlayerUI : MonoBehaviour
                              $"Turn on Generators: {_levelManager.generatorCount}/{_levelManager.generatorObjective} \n \n" +
                              $"Turn on Lamps: {_levelManager.lampCount}/{_levelManager.lampObjective} \n \n" +
                              $"Defeat Tokoloshes: {_levelManager.enemyCount}/{_levelManager.enemyObjective}";
-        
+    }
+
+
+    void objectiveTab()
+    {
+        if (isObjectiveOpen)
+        {
+            StartCoroutine(SlideObjectiveTab(hideRectPosY));
+        }
+        else
+        {
+            StartCoroutine(SlideObjectiveTab(showRectPosY));
+        }
+
+        isObjectiveOpen = !isObjectiveOpen;
+    }
+
+    IEnumerator SlideObjectiveTab(float yEnd)
+    {
+        var objectiveTransformPos = objectiveTransform.anchoredPosition;
+        float yStart = objectiveTransformPos.y;
+
+        float t = 0;
+
+        while (t < 1)
+        {
+            Debug.Log(t);
+            t += Time.deltaTime;
+            if (t > 1) t = 1;
+            float newY = Mathf.Lerp(yStart, yEnd, t);
+            objectiveTransform.anchoredPosition = new Vector2(objectiveTransformPos.x, newY);
+            yield return null;
+        }
         
     }
+    
 }
