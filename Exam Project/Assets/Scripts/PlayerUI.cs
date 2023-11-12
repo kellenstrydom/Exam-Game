4819b5693be2a4cc7bd8b07b13fa0921ced3a1a7
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2023-11-12 15:25:32 +0200

    meta

--- Exam Project/Assets/Scripts/PlayerUI.cs
+++ Exam Project/Assets/Scripts/PlayerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,35 @@ public class PlayerUI : MonoBehaviour
     [SerializeField]
     private Slider chargeSlider;
 
-    public PlayerInfo _playerInfo;
+    [SerializeField] private Text objectiveText;
+
+    private PlayerInfo _playerInfo;
+    private LevelManager _levelManager;
+
+    private void Start()
+    {
+        _playerInfo = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInfo>();
+        _levelManager = GameObject.FindGameObjectWithTag("Level Manager").GetComponent<LevelManager>();
+    }
 
     void Update()
     {
         healthSlider.value = (int)(_playerInfo.HealthPoints / _playerInfo.MaxHealthPoints * 100);
         chargeSlider.value = (int)(_playerInfo.charge / _playerInfo.maxCharge * 100);
+    }
+
+    void DisplayObjectives()
+    {
+        string completeMsg;
+        if (_levelManager.objectivesComplete)
+            completeMsg = "Area"
+        
+        objectiveText.text = $"Objectives: \n \n" +
+                             $"Turn on Generators: {_levelManager.generatorCount}/{_levelManager.generatorObjective} \n" +
+                             $"Turn on Lamps: {_levelManager.lampCount}/{_levelManager.lampObjective} \n" +
+                             $"Defeat Tokoloshes: {_levelManager.enemyCount}/{_levelManager.enemyObjective} \n" +
+                             $"";
+        
+        
     }
 }
