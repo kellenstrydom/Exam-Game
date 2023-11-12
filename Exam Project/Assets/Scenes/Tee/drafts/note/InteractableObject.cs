7cf0a96b85cf130a6442de8e9822277de62f0db0
ABMEHT <nei29116@gmail.com>
2023-11-12 18:24:31 +0200

    Revert "prefabs"
    This reverts commit caf894f2eee22aae6c1697bbfbef1d778b1219ef.

--- Exam Project/Assets/Scenes/Tee/drafts/note/InteractableObject.cs
+++ Exam Project/Assets/Scenes/Tee/drafts/note/InteractableObject.cs
@@ -8,20 +8,5 @@ public class InteractableObject : MonoBehaviour
     public bool hasImage = false;
     public string noteText = "This is a note.";
     public Sprite image;
-    public GameObject note;
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag != "Player")
-            return;
-        note.SetActive(true);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag != "Player")
-            return;
-        note.SetActive(false);
-    }
 
 }
