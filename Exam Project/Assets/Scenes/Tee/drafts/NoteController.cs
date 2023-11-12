90f7a5d30596303a65e27ed6aec8beba5e8c1d53
ABMEHT <nei29116@gmail.com>
2023-11-12 12:01:59 +0200

    update

---
+++ Exam Project/Assets/Scenes/Tee/drafts/NoteController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("Input")]
+    [SerializeField] private KeyCode closeKey;
+
+    public void ShowNote()
+    {
+
+    }
+}
