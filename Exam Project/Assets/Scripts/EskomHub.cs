1f5fb8c692db5c0192b72742d89e0480069bb975
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2023-11-13 13:05:42 +0200

    Create EskomHub.cs

---
+++ Exam Project/Assets/Scripts/EskomHub.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EskomHub : MonoBehaviour
+{
+    public static bool isCity1DoorOpen;
+    
+    
+    public void GoToCity1()
+    {
+        
+    }
+}
